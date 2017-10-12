1572ec786b47fac446acdf4a4fb99188ae54b0a3
xianhuawei <xianhuawei.zhcn@qq.com>
2017-10-13 06:44:06 +0800

    CrateDB

--- services/CRATEDB/package/scripts/service_check.py
+++ services/CRATEDB/package/scripts/service_check.py
@@ -13,16 +13,6 @@ class ServiceCheck(Script):
 
         print("Running CrateDB service check", file=sys.stdout)
         payload = {'name': 'Buddy.  Dont Worry, I am Fine '}
-        pass
-        # r = requests.get('http://localhost:' + params.http_port + '/', params=payload)
-        #
-        # if r.status_code == 200:
-        #     print(r.json(), file=sys.stdout)
-        #     sys.exit(0)
-        # else:
-        #     print("CrateDB is not running", file=sys.stderr)
-        #     sys.exit(1)
-
 
 if __name__ == "__main__":
     ServiceCheck().execute()
